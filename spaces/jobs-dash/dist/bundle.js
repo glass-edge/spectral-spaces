743b8d3a10bc87a726c34dab8b7d1a760a07832b
Dzmitry Mazouka <sdkedim@gmail.com>
2025-01-20 19:06:18 +1300

    Implement basic jobs-dash function

--- spaces/jobs-dash/dist/bundle.js
+++ spaces/jobs-dash/dist/bundle.js
@@ -15,10 +15,44 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 function init(value) {
-    const element = document.getElementById('value');
-    if (element != null) {
-        element.innerHTML = JSON.stringify(value, null, 2);
+    var _a, _b, _c;
+    const element = document.getElementById('container');
+    if (element == null) {
+        return;
     }
+    const repositories = new Map();
+    for (const job of value.jobs) {
+        if (repositories.has(job.repo)) {
+            repositories.get(job.repo).push(job);
+        }
+        else {
+            repositories.set(job.repo, [job]);
+        }
+    }
+    let html = '';
+    for (const [repo, jobs] of repositories) {
+        jobs.sort((a, b) => {
+            return a.number - b.number;
+        });
+        const lastJob = jobs[jobs.length - 1];
+        const status = lastJob.conclusion === 'success' ? 'status-success'
+            : lastJob.conclusion === 'failure' ? 'status-fail'
+                : 'status-in-progress';
+        html += `
+      <div class="card m-2" style="width: 18rem;">
+        <div class="card-img-top status ${status}">
+          <a href="${lastJob.url}">
+            ${(_c = (_b = ((_a = lastJob.conclusion) !== null && _a !== void 0 ? _a : lastJob.status)) === null || _b === void 0 ? void 0 : _b.toUpperCase()) !== null && _c !== void 0 ? _c : ''}
+          </a>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${repo}</h5>
+          <p class="card-text job-text">${lastJob.author}<br/>${lastJob.commit}</p>
+        </div>
+      </div>
+    `;
+    }
+    element.innerHTML = html;
 }
 window.electron.onRenderValue((value) => __awaiter(void 0, void 0, void 0, function* () {
     init(value);
--- spaces/jobs-dash/src/main.ts
+++ spaces/jobs-dash/src/main.ts
@@ -1,8 +1,58 @@
+type Job = {
+  repo: string
+  name: string
+  event: string
+  status: string
+  conclusion: string
+  number: number
+  created: string
+  updated: string
+  title: string
+  url: string
+  commit: string
+  author: string
+}
+
 function init (value: any) {
-  const element = document.getElementById('value')
-  if (element != null) {
-    element.innerHTML = JSON.stringify(value, null, 2)
+  const element = document.getElementById('container')
+  if (element == null) {
+    return
   }
+
+  const repositories = new Map<string, Job[]>()
+  for (const job of value.jobs) {
+    if (repositories.has(job.repo)) {
+      repositories.get(job.repo)!.push(job)
+    } else {
+      repositories.set(job.repo, [job])
+    }
+  }
+
+  let html = ''
+  for (const [repo, jobs] of repositories) {
+    jobs.sort((a, b) => {
+      return a.number - b.number
+    })
+    const lastJob = jobs[jobs.length - 1]
+    const status = lastJob.conclusion === 'success' ? 'status-success'
+      : lastJob.conclusion === 'failure' ? 'status-fail'
+      : 'status-in-progress'
+
+    html += `
+      <div class="card m-2" style="width: 18rem;">
+        <div class="card-img-top status ${status}">
+          <a href="${lastJob.url}">
+            ${(lastJob.conclusion ?? lastJob.status)?.toUpperCase() ?? ''}
+          </a>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${repo}</h5>
+          <p class="card-text job-text">${lastJob.author}<br/>${lastJob.commit}</p>
+        </div>
+      </div>
+    `
+  }
+  element.innerHTML = html
 }
 
 (window as any).electron.onRenderValue(async (value: any) => {
